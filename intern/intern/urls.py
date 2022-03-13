bc3efeccfea4b93545587ae3f0c039c3ecc88515
Poottipong Rueangsuntikornkul <6210520117@studen.tu.ac.th>
2022-03-13 16:21:38 +0700

    initial commit

--- intern/intern/urls.py
+++ intern/intern/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from user import views as views1
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('', include())
 ]
-
-urlpatterns += staticfiles_urlpatterns()
