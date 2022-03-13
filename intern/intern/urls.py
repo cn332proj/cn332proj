6ca4f40785abc7ea34e17dee472b3f31452b677b
Jarukorn Lumchom <6210612708@cn103>
2022-03-13 15:54:11 +0700

    update

--- intern/intern/urls.py
+++ intern/intern/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from user import views as views1
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views1.user),
+    path('user/', include('user.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
---
+++ intern/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name="user"
+urlpatterns = [
+    # path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+]
